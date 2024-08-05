f1aea4bf60cbdaeff741ecd1675f52773b406cc3
USER 1 <gchkumar999@gmail.com>
2024-08-05 23:16:48 +0530

    txt

---
+++ ReactJS/Backup/class-3/src/App.jsx
@@ -0,0 +1,13 @@
+import Message from './message/Message'
+
+let App=()=>{
+    return <div>
+        <h2>App Component</h2>
+        <hr/>
+        <Message/>
+        <Message/>
+        <Message/>
+    </div>
+}
+
+export default App
