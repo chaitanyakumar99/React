bfd976137e36bed0fd22f473383d9f8636cb9d46
USER 1 <gchkumar999@gmail.com>
2024-08-05 23:18:55 +0530

    txt

---
+++ ReactJS/Backup/class-3/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+//ReactDOM.render("Good Evening",document.getElementById('root'))
+//ReactDOM.render(<h1>Good Evening</h1>,document.getElementById('root'))
+//ReactDOM.render(<h1>Good Evening</h1><h2>Welcome</h2>,document.getElementById('root'))
+//ReactDOM.render(<><h1>Good Evening</h1><h2>Welcome</h2></>,document.getElementById('root'))
+ReactDOM.render(<App/>,document.getElementById('root'))
