5ac416d71a653f7d8998fbcb4b9f23c676581d30
USER 1 <gchkumar999@gmail.com>
2024-08-07 17:04:25 +0530

    txt

---
+++ ReactJS/react7/src/App.js
@@ -0,0 +1,10 @@
+import Header from "./components/Header";
+
+let App=()=>{
+    return <div>
+        <h1>welcome</h1>
+        <Header eid={456}/>
+    </div>
+}
+
+export default App
---
+++ ReactJS/react7/src/components/Header.jsx
@@ -0,0 +1,9 @@
+let Header=(props)=>{
+    return <div>
+    <h1>hi this is message component</h1>
+    <pre>{JSON.stringify(props)}</pre>
+         
+    </div>
+}
+
+export default Header
