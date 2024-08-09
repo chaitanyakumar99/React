e6072615f0dd6af52caac852419d9d3a425637f1
USER 1 <gchkumar999@gmail.com>
2024-08-09 22:31:57 +0530

    txt

---
+++ ReactJS/Backup/class-product/src/App.js
@@ -0,0 +1,9 @@
+import Product from './components/Product'
+
+let App=()=>{
+    return <div>
+        <h1>THis is a App component</h1>
+        <Product/>
+    </div>
+}
+export default App
---
+++ ReactJS/Backup/class-product/src/components/Cart.jsx
@@ -0,0 +1,11 @@
+let Cart = (props) => {
+  return (
+    <div>
+        <h2>cart componet</h2>
+        <pre>{JSON.stringify(props)}</pre>
+    </div>
+  )
+}
+export default Cart
+
+
