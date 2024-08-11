cdb15ac8fb4430cee1c445a5f2271b8a8aabc6fa
USER 1 <gchkumar999@gmail.com>
2024-08-11 10:21:19 +0530

    txt

--- ReactJS/Backup/class-5/src/App.js
+++ ReactJS/Backup/class-5/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-// import Footer from './website/Footer'
-// import Header from './website/Header'
-// import Slider from './website/Slider'
-// import Movie from "./Movies/Movie";
+import Footer from './website/Footer'
+import Header from './website/Header'
+import Slider from './website/Slider'
+import Movie from "./Movies/Movie";
 import Movie from "./Movies/Movie1";
 
 class App extends React.Component{
@@ -10,6 +10,9 @@ class App extends React.Component{
         return <div>
             <h1>App Component</h1>
             <Movie/>
+            <Footer/>
+            <Header/>
+            <Slider/>
         </div>
     }
 }
