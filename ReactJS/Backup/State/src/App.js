a76ad8e1175324e9cd13e6ba004f1b5b25cd4e74
USER 1 <gchkumar999@gmail.com>
2024-08-12 18:15:33 +0530

    txt

---
+++ ReactJS/Backup/State/src/App.js
@@ -0,0 +1,11 @@
+import react from 'react'
+import One from './Components/One'
+class App extends react.Component{
+    render(){
+        return <div>
+            <h1>App component</h1>
+            <One/>
+        </div>
+    }
+}
+export default App
---
+++ ReactJS/Backup/State/src/Components/One.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+
+class One extends react.Component{
+    state={
+        msg:"Hello"
+    }
+   gmHandler=()=>{
+    this.setState({msg:"goood morning"})
+   }
+    render(){
+        return <div>
+                  <h1>This is a One comonent</h1>
+                  <h2>msg:{this.state.msg}</h2>
+                  <button onClick={this.gmHandler}>clickme</button>
+               </div>
+    }
+}
+export default One
