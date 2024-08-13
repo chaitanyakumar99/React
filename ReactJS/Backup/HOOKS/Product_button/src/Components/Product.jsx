96cbe4bb99557ef73b6377d4aa362a0911d579c2
USER 1 <gchkumar999@gmail.com>
2024-08-13 21:01:03 +0530

    txt

--- ReactJS/Backup/HOOKS/Product_button/src/Components/Product.jsx
+++ ReactJS/Backup/HOOKS/Product_button/src/Components/Product.jsx
@@ -1,12 +1,12 @@
 import { useState ,useEffect } from "react"
 
 let Product=()=>{
-           let[msg, chaithanya]=useState("Hello");
-           let [qty,Siva]=useState("Come to institute")
+           let[msg, ck]=useState("Hello");
+           let [qty,kk]=useState("Come to institute")
 
            
            let gmHandler=()=>{
-            chaithanya("Goodmorning")
+            ck("Goodmorning")
           };
 
           // useEffect(()=>{
@@ -18,10 +18,10 @@ let Product=()=>{
                     <h1>This is a Product component</h1>
                     <h2>Message:{msg}</h2>
                     <button onClick={gmHandler}>GM</button>
-                    <button onClick={()=>{chaithanya("Good night")}
+                    <button onClick={()=>{ck("Good night")}
             }>GN</button>
                <h1>{qty}</h1>
-          <button onDoubleClick={()=>{Siva("Upgrade the Skills")}}>Clickme</button>
+          <button onDoubleClick={()=>{kk("Upgrade the Skills")}}>Clickme</button>
                   </>
                   
 }
