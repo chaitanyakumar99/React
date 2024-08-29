02ecef976e85f8836a1ae1117c545c960fb1d2ae
USER 1 <gchkumar999@gmail.com>
2024-08-29 22:52:49 +0530

    txt

---
+++ ReactJS/react7/src/Compo/reg.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const reg = () => {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-4'>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default reg
