7059d027aa139d7f12e121e240f802b650a319f3
chaitanyakumar99 <gchkumar999@gmail.com>
2024-10-14 22:05:26 +0530

    txt

---
+++ ReactJS-Practice/class1/src/App.js
@@ -0,0 +1,6 @@
+let App=()=>{
+
+    return <h1>Hello,Good Evenning</h1>
+}
+
+export default App
---
+++ ReactJS-Practice/class1/src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App/>,document.getElementById("root"))
+
+// ReactDOM.render(<div><h1>Hello,GM</h1><h2>welcome to react</h2></div>,document.getElementById('root')) 
