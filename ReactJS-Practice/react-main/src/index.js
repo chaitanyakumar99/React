34870a467782864821a5bcfe74cdd352500e84a2
chaitanyakumar99 <gchkumar999@gmail.com>
2024-10-14 21:44:57 +0530

    txt

--- ReactJS-Practice/react-main/src/index.js
+++ ReactJS-Practice/react-main/src/index.js
@@ -1,3 +1,3 @@
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello,GM</h1>,document.getElementById('root'))
+ReactDOM.render(<div><h1>Hello,GM</h1><h2>Hello,GM</h2></div>,document.getElementById('root'))
