aea03d5f3dac6080064014a38c57f80c8fb934a5
chaitanyakumar99 <gchkumar999@gmail.com>
2024-10-14 22:31:06 +0530

    txt

--- ReactJS-Practice/react-main/src/index.js
+++ ReactJS-Practice/react-main/src/index.js
@@ -1,3 +1,5 @@
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<div><h1>Hello,GM</h1><h2>welcome to react</h2></div>,document.getElementById('root')) 
+ReactDOM.render(<App/>,document.getElementById("root"))
+
