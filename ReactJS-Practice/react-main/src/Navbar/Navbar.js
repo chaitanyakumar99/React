9f35f74aa543b449608d38d681998fd24e652984
chaitanyakumar99 <gchkumar999@gmail.com>
2024-10-16 21:55:17 +0530

    txt

--- ReactJS-Practice/react-main/src/Navbar/Navbar.js
+++ ReactJS-Practice/react-main/src/Navbar/Navbar.js
@@ -2,8 +2,24 @@ import React from "react";
 
 class Navbar extends React.Component {
     render() {
-        return <nav>
-            <a href="#">React EXample</a>
+        return <nav className="navbar navbar-dark bg-dark navbar-expand-lg" >
+            <a href="#" className="navbar-brand">React Example</a>
+            <div className="ml-auto">
+                <ul className="navbar-nav">
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">Services</a>
+                    </li>
+                </ul>
+            </div>
         </nav>
     }
 }
