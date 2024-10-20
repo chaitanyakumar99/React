a6190298da8fa1b6c765e8f1ec75a7bdfe99a23f
chaitanyakumar99 <gchkumar999@gmail.com>
2024-10-20 22:15:37 +0530

    txt

---
+++ ReactJS-Practice/class4 react-example/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Message from './Message/Message'
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <Message />
+            
+        </div>
+    )
+}
+
+export default App
---
+++ ReactJS-Practice/class4 react-example/src/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+class Navbar extends React.Component {
+    render() {
+        return <nav className="navbar navbar-dark bg-dark navbar-expand-lg" >
+            <a href="#" className="navbar-brand">React Example</a>
+
+            <div className="ml-auto"> 
+
+                <ul className="navbar-nav">
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">Contact</a>
+                    </li>
+                    <li className="nav-list">
+                        <a className="nav-link" href="#">Services</a>
+                    </li>
+                </ul>
+
+            </div>
+
+        </nav>
+    }
+}
+
+export default Navbar
