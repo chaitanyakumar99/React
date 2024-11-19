b699d8b752cac5c2a4cf0f649efd715896bdc1fe
chaitanyakumar99 <gchkumar999@gmail.com>
2024-11-19 19:29:11 +0530

    txt

--- ReactJS-Practice/react-main/src/App.js
+++ ReactJS-Practice/react-main/src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
-import Message from "./Message/Message";
+// import Message from "./Message/Message";
+import Navbar from "./Navbar/Navbar";
 let App = () => {
   return <div>
 
-    <h2>App Component</h2>
-    <hr />
-    <Message />
+    {/* <h2>App Component</h2> */}
+ 
+    {/* <Message /> */}
+    {/* <button className="btn btn-warning">Text</button> */}
+    <Navbar/>
+
   </div>  
 }
 
-export default App 
+export default App  
+
+
+
+
---
+++ ReactJS-Practice/react-main/src/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+let Navbar = () => {
+
+    return <div>
+        <nav className="navbar navbar-dark bg-dark navbar-expand-lg ">
+            <a href="#" className="navbar-brand">Bootstrap</a>
+            <div className="ml-auto"> 
+                <ul className="navbar-nav">
+                    <li><a href="#" className="nav-link">Home</a></li>
+                    <li><a href="#" className="nav-link" >About</a></li>
+                    <li><a href="#" className="nav-link">Services</a></li>
+                    <li><a href="#" className="nav-link">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+}
+
+export default Navbar 
