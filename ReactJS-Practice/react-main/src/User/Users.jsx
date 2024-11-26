5b16b92d652666dc6ae46a0d90d8f03fb899fb36
chaitanyakumar99 <gchkumar999@gmail.com>
2024-11-26 23:56:11 +0530

    txt

--- ReactJS-Practice/react-main/src/User/Users.jsx
+++ ReactJS-Practice/react-main/src/User/Users.jsx
@@ -2,24 +2,24 @@ import React, { Component } from 'react'
 import Axios from 'axios'
 
 class Users extends Component {
-    users=[];
-    constructor(props){
+    users = [];
+    constructor(props) {
         super(props)
-        this.state={
-            users:[]
+        this.state = {
+            users: []
         }
     }
     getUserData = () => {
         Axios.get('https://jsonplaceholder.typicode.com/users')
             .then((resp) => {
                 console.log(resp.data)
-                this.setState({users:resp.data}) 
+                this.setState({ users: resp.data })
             })
             .catch()
     }
     render() {
         return (
-            <div>  
+            <div>
                 <pre>{JSON.stringify(this.state.users)}</pre>
                 <h2>User Component </h2>
                 <button onClick={this.getUserData}>Get User Data</button>
@@ -29,14 +29,14 @@ class Users extends Component {
                         <tr>
                             <th>Id</th>
                             <th>Name</th>
-                            <th>Email</th>
+                            <th>Email</th> 
                         </tr>
-                        <tbody>
-                            <tr>
-                                  <td></td>
-                            </tr>
-                        </tbody>
                     </thead>
+                    <tbody>
+                      {
+                        this.state.users.length >0 ? <div></div>:<div><h3>No Data</h3></div>
+                      }
+                    </tbody> 
                 </table>
             </div>
         )
