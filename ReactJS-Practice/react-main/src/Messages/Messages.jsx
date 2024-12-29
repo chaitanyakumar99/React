7721b4abe87d409e9d9ffc9f041fe609af1efb2b
chaitanyakumar99 <gchkumar999@gmail.com>
2024-12-29 23:46:07 +0530

    txt

--- ReactJS-Practice/react-main/src/Messages/Messages.jsx
+++ ReactJS-Practice/react-main/src/Messages/Messages.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Messages = () => {
   return (
     <div>
-        <h2>Message component</h2>
+        <h2>Message component...!</h2>
         <hr />
     </div>
   )
